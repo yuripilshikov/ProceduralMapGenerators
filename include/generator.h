c5e93bc8297aaa12d24d3b5737b846eb73233bbd
YuriPilshchikov <yuri.nikolaevitch@gmail.com>
2026-07-07 20:45:53 +0300

    Smooth map algorythm added

--- include/generator.h
+++ include/generator.h
@@ -6,5 +6,21 @@ void someFunc();
 
 int lib_add(int a, int b);
 
-// Random map
+/**
+	\brief Creates random map of zeros and ones
+	\param[in] width Desired width of map
+	\param[in] height Desired height of map
+	\return Created map (vector of ints, by value)
+*/
 std::vector<int> createRandomMap(int width, int height);
+
+/**
+	\brief Smoothes random map (celluar automata algorythm)
+	\param[in] width Desired width of map
+	\param[in] height Desired height of map
+	\param[in] map Random map to smooth
+	\param[in] times How many times to smooth
+*/
+
+std::vector<int> smoothMap(int width, int height, 
+	std::vector<int> const& map, int times);
--- src/generator.cpp
+++ src/generator.cpp
@@ -20,3 +20,38 @@ std::vector<int> createRandomMap(int width, int height)
 	}
 	return randomMap;
 }
+
+std::vector<int> smoothMap(int width, int height, 
+	std::vector<int> const& map, int times)
+{
+	std::vector<int> smooth(map.size());
+	int height = map.size() / width;
+
+	for(int time = 0; time < times; ++time)
+	{
+		for(int x = 0; x < width; ++x)
+		{
+			for(int y = 0; y < height; ++y)
+			{
+				int floors = 0;
+				int rocks = 0;
+
+				for(int ox = -1; ox <= 1; ++ox)
+				{
+					for(int oy = -1; oy <= 1; ++oy)
+					{
+						if(x + ox < 0 
+							|| x + ox >= width 
+							|| y + oy < 0 
+							|| y + oy >= height) 
+								continue;
+						if(map[x + ox + (y + oy) * width] == 0) ++floors;
+						else ++ rocks;
+					}
+				}
+				smooth[x + y * width] = floors >= rocks ? 0 : 1;
+			}
+		}
+	}
+	return smooth;
+}
